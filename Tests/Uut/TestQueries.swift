82239473bd416dd39906f433b8cc98eb2f4942e9
Luke Matthew Sutton <me@lukematthewsutton.com>
2016-04-29 20:57:39 +0930

    Add some more query tests

--- Tests/Uut/TestQueries.swift
+++ Tests/Uut/TestQueries.swift
@@ -40,4 +40,24 @@ class TestQueries: XCTestCase {
 
     XCTAssertEqual(compiler.compile(styles), expected)
   }
+
+  func testNot() {
+    let query = m.not(m.screen |& m.maxHeight(400.em))
+    XCTAssertEqual(query.stringValue, "not screen and (max-height: 400em)")
+  }
+
+  func testAnd() {
+    let query = m.handheld |& m.minWidth(600.px) |& m.maxWidth(800.px)
+    XCTAssertEqual(query.stringValue, "handheld and (min-width: 600px) and (max-width: 800px)")
+  }
+
+  func testOr() {
+    let query = m.handheld || m.screen
+    XCTAssertEqual(query.stringValue, "handheld , screen")
+  }
+
+  func testComplicated() {
+    let query = m.handheld |& m.minWidth(500.px) || m.screen |& m.minWidth(1200.px) |& m.maxWidth(2000.px)
+    XCTAssertEqual(query.stringValue, "handheld and (min-width: 500px) , screen and (min-width: 1200px) and (max-width: 2000px)")
+  }
 }
